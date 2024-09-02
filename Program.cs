224a5c212f8cf9de91aed974a9cfafab89611345
c4t1sf4ct10n <mathieu.chatons@gmail.com>
2024-09-02 14:11:43 +0200

    1st commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+public class RaylibGame
+{
+    public static int Main()
+    {
+        Console.WriteLine("Je suis passé par là");
+        InitWindow(800, 600, "Premier programme Raylib !");
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Color.White);
+            EndDrawing();
+        }
+
+        CloseWindow();
+        return 0;
+    }
+}
